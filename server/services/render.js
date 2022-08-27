ff7cb36e4608508be3ca3ce24edbf28fd17579c8
SHIVANIUM_ <shivrathore223@gmail.com>
2022-08-27 13:25:25 +0530

    second change

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://shiv-crud-app/api/users')
+    axios.get('http://shiv-crud-app.herokuapp.com/api/users')
         .then(function(response){
             res.render('index', { users : response.data });
         })
@@ -11,7 +11,6 @@ exports.homeRoutes = (req, res) => {
             res.send(err);
         })
 
-    
 }
 
 exports.add_user = (req, res) =>{
@@ -19,7 +18,7 @@ exports.add_user = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('http://shiv-crud-app/api/users', { params : { id : req.query.id }})
+    axios.get('http://shiv-crud-app.herokuapp.com/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
