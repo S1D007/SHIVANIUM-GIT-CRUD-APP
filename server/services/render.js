1bf2056127fdd41443e973dc01c8edc80c35e7f9
Siddhant <94226898+S1D007@users.noreply.github.com>
2022-08-28 10:14:45 +0530

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('/api/users')
+    axios.get('https://shivanium-git-crud-app-production.up.railway.app/api/users')
         .then(function(response){
             res.render('index', { users : response.data });
         })
@@ -18,7 +18,7 @@ exports.add_user = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('/api/users', { params : { id : req.query.id }})
+    axios.get('https://shivanium-git-crud-app-production.up.railway.app/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
