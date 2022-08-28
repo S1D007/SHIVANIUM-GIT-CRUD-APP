d416469a22e1ff09a913a3bbf6f097517bbc97d9
Siddhant <94226898+S1D007@users.noreply.github.com>
2022-08-28 10:13:15 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `http://shivanium-git-crud-app-production.up.railway.app/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +34,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `http://shivanium-git-crud-app-production.up.railway.app/api/users/${id}`,
             "method" : "DELETE"
         }
 
